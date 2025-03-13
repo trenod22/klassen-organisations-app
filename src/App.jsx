071efd9e654970354a6b2f481a8105495d446756
trenod22 <trenod22@htl-kaindorf.at>
2025-03-13 15:18:35 +0100

    added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import MainSite from "./components/MainSite.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [count, setCount] = useState(0)
